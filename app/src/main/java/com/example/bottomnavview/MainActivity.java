314750a2a2aa8b828bf8968fc13c5020fea0a7d3
curvatia <curvatia2002@yahoo.es>
2022-01-30 16:14:16 +0100

    ADD activity_login.xml

--- app/src/main/java/com/example/bottomnavview/MainActivity.java
+++ app/src/main/java/com/example/bottomnavview/MainActivity.java
@@ -1,18 +1,25 @@
 package com.example.bottomnavview;
 
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.view.View;
 
+import com.example.bottomnavview.ui.dashboard.DialogoFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.bottomnavview.databinding.ActivityMainBinding;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import android.app.FragmentTransaction;
+import com.google.android.material.snackbar.Snackbar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
 
     //BOTTOMNAVIGATIONVIEW
     private ActivityMainBinding binding;
@@ -37,8 +44,41 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupWithNavController(binding.navView, navController);
         //END BOTTOMNAVIGATIONVIEW
 
+        //FAB
+        /*FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });*/
+        //END FAB
 
+        /*final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(this);*/
 
     }
+
+    /*@Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.fab:
+                showCreateDialog();
+                break;
+        }
+    }*/
+
+    /*private void showCreateDialog() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        DialogoFragment newFragment = new DialogoFragment();
+
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+        transaction.add(android.R.id.content, newFragment)
+                .addToBackStack(null).commit();
+    }*/
+
+
 
 }
